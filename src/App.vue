ef46894985825dfc5d86f28c1e70eb3aeae332a6
Axel David <axeldvi.pro@gmail.com>
2024-12-30 01:37:01 +0100

    add basic footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue'
 import ExperienceComponent from './components/ExperienceComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import ExperienceComponent from './components/ExperienceComponent.vue'
   >
     <HeaderComponent />
     <ExperienceComponent />
+    <FooterComponent />
   </div>
 </template>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import linkedinLogo from '@/assets/linkedin.svg'
+</script>
+
+<template>
+  <footer class="flex flex-row items-center justify-center p-4">
+    <a
+      href="https://www.linkedin.com/in/axel-david-297346143/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        :src="linkedinLogo"
+        alt="LinkedIn logo"
+        class="size-12"
+      >
+    </a>
+  </footer>
+</template>
