2c899cb7984b4a9c0dd998208154880cd5deec95
Axel David <axeldvi.pro@gmail.com>
2024-12-30 01:42:53 +0100

    update intro text

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,5 +1,5 @@
 export default Object.freeze({
     fullname: 'Axel David',
     title: 'Full Stack Developer',
-    intro: 'I am a Full Stack Developer with 3 years of experience in web development. I am passionate about building scalable and maintainable web applications.'
+    intro: 'Full Stack Developer with 3 years of experience in web development, passionate about building scalable and maintainable web applications.'
 })
--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -1,5 +1,5 @@
 export default Object.freeze({
     fullname: 'Axel David',
     title: 'Développeur Full Stack',
-    intro: 'Je suis un développeur Full Stack avec 3 ans d\'expérience dans le développement web. Je suis passionné par la construction d\'applications web évolutives et maintenables.'
+    intro: 'Développeur Full Stack avec 3 ans d\'expérience en développement web, passionné par la construction d\'applications web évolutives et maintenables.'
 })
